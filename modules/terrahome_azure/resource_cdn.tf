c20adf5399a753378c66038da6d0b476b110b91c
Matthew Flynn <mpflynnx@gmail.com>
2023-11-01 09:56:43 +0000

    #26 Implemented CDN using Azure Front Door

--- modules/terrahome_azure/resource_cdn.tf
+++ modules/terrahome_azure/resource_cdn.tf
@@ -1,12 +1,3 @@
-# resource "random_pet" "rg-name" {
-#   prefix = var.resource_group_name_prefix
-# }
-
-# resource "azurerm_resource_group" "rg" {
-#   name     = random_pet.rg-name.id
-#   location = var.resource_group_location
-# }
-
 resource "random_id" "front_door_endpoint_name" {
   byte_length = 8
 }
