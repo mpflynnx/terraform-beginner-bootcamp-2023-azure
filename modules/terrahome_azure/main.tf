20327931902260a642252052eab868bd25837215
Matthew Flynn <mpflynnx@gmail.com>
2023-11-27 10:22:14 +0000

    #44 Updated provider versions

--- modules/terrahome_azure/main.tf
+++ modules/terrahome_azure/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.76.0"
+      version = "~>3.82.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~>3.5.1"
     }
   }
 }
